8f0f7c9ff84cb3ec9c1e8a1f7051ee930ad1434b
Matteo 'Peach' Pescarin <peach@smartart.it>
2019-01-26 16:53:29 +0000

    created store and initialised store Provider
    - cleaned up App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+import weatherWidgetReducers from "./redux/reducers";
+
+//import * as serviceWorker from "./serviceWorker";
+const loggerMiddleware = createLogger();
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  weatherWidgetReducers,
+  composeEnhancer(applyMiddleware(thunkMiddleware, loggerMiddleware))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
